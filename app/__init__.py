2446d1e7e9e8c24eae2e28a58958e9698f48e2e1
rubiali-windows <gabrielrubiali@gmail.com>
2025-12-26 14:11:37 -0300

    add health check route and remove unused main route

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,11 @@ def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    from app.routes.main import main_bp
     from app.routes.logs_routes import logs_bp
+    from app.routes.health_routes import health_bp
 
-    app.register_blueprint(main_bp)
     app.register_blueprint(logs_bp)
+    app.register_blueprint(health_bp)
 
     with app.app_context():
         setup_indexes()
--- app/routes/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-main_bp = Blueprint("main_bp", __name__)
-
-@main_bp.route("/")
-def hello_world():
-    return {"message": "Hello World!"}
