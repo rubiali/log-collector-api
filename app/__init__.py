5eba05c7f28f39041136f2682f8e825134385d6b
rubiali-windows <gabrielrubiali@gmail.com>
2025-12-26 12:59:28 -0300

    adjusting order config

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from .config.settings import DevelopmentConfig
 
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     from app.routes.main import main_bp
     app.register_blueprint(main_bp)
-    app.config.from_object(config_class)
     
     return app
